09f711a68c14864bc2868131f9884250ef68c808
sjuknelis <81927579+sjuknelis@users.noreply.github.com>
2022-10-28 09:27:53 -0400

    DFS + dijkstra modified

--- tree.py
+++ tree.py
@@ -1,7 +1,6 @@
 from functools import cmp_to_key
-import random
+import random,math
 
-from matplotlib.table import Table
 from better_prob import Card
 from main import score_possible_hands
 
@@ -14,7 +13,11 @@ class SuitNode:
   def get_children(self):
     results = []
     for i in range(13):
-      odds = 4 - self.numbers_seen.count(i)
+      odds = 1
+      for (suit,number) in zip(self.suits_seen,self.numbers_seen):
+        if suit == self.value and number == i:
+          odds = 0
+          break
       results.append((NumberNode(i,self.suits_seen,self.numbers_seen + [i],self),odds))
     results.sort(key=cmp_to_key(lambda a,b: b[1] - a[1]))
     return results
@@ -29,49 +32,135 @@ class NumberNode:
     results = []
     for i in range(4):
       odds = 13 - self.suits_seen.count(i)
-      results.append((SuitNode(i,self.suits_seen,self.numbers_seen + [i],self),odds))
+      results.append((SuitNode(i,self.suits_seen + [i],self.numbers_seen,self),odds))
     results.sort(key=cmp_to_key(lambda a,b: b[1] - a[1]))
     return results
 
-def simulate(node,hand,table_shown):
+def simulate(node):
   cards = []
-  for _ in range(9 - len(table_shown)):
-    number = node.value
-    suit = node.parent.value
+  for (suit,number) in zip(node.suits_seen,node.numbers_seen):
     cards.append(Card(suit,number))
-    node = node.parent.parent
-  table = table_shown + cards[:5 - len(table_shown)]
+  table = cards[2:7]
   players = [
-    hand,
-    cards[5 - len(table_shown):7 - len(table_shown)],
-    cards[7 - len(table_shown):]
+    cards[:2],
+    cards[7:9],
+    cards[9:]
   ]
+  print(cards)
   scores = [score_possible_hands(player,table) for player in players]
   return scores
 
 def select_random_child(node):
   children = node.get_children()
   total = sum([child[1] for child in children])
-  print(total)
   value = random.randint(1,total)
   for (child,odds) in children:
     value -= odds
     if value <= 0:
       return child
 
-def monte_carlo_from_node(node,hand,table_depth,table_shown,sim_count):
+def monte_carlo_from_node(node,table_depth,sim_count):
   wins = 0
   original_node = node
   for _i in range(sim_count):
-    for _j in range(9 - table_depth):
+    print(len(node.suits_seen))
+    for _j in range(11 - len(node.suits_seen)):
       node = select_random_child(node)
       node = select_random_child(node)
-    scores = simulate(node,hand,table_shown)
-    print(scores)
+      print(_j)
+    scores = simulate(node)
     if scores[0] == max(scores):
       wins += 1
     node = original_node
   return wins / sim_count
 
-node = NumberNode(0,[0,0,1,1,1],[12,11,12,11,10],None).get_children()[0][0].get_children()[0][0].get_children()[0][0].get_children()[0][0]
-print(monte_carlo_from_node(node,[Card(0,12),Card(0,1)],3,[Card(1,12),Card(1,11),Card(1,10)],50))
+def dfs_list(node,max_count,max_depth):
+  result = set()
+  def dfs(node,depth):
+    if depth >= max_depth:
+      result.add(node)
+      return
+    for (child,odds) in node.get_children():
+      if len(result) >= max_count:
+        return
+      if child not in result:
+        dfs(child,depth + 1)
+  dfs(node,0)
+  return result
+
+def djikstra_mod_list(node,max_count):
+  found_nodes = []
+  heap = Heap(node.get_children())
+  while len(found_nodes) < max_count:
+    (chosen_node,chosen_odds) = heap.pop()
+    found_nodes.append(chosen_node)
+    for (child_node,child_odds) in chosen_node.get_children():
+      heap.insert((child_node,chosen_odds + child_odds))
+  return found_nodes
+
+class Heap:
+  data = []
+  def __init__(self,data):
+    for item in data:
+      self.insert(item)
+  def bubble_up(self,index):
+    parent_index = index
+    while parent_index > 0:
+      current_index = parent_index
+      parent_index = math.floor((current_index - 1) / 2)
+      if self.data[parent_index][1] < self.data[current_index][1]:
+        old_parent = self.data[parent_index]
+        self.data[parent_index] = self.data[current_index]
+        self.data[current_index] = old_parent
+      else:
+        return
+  def push_down(self,index):
+    current_index = index
+    depth = math.floor(math.log2(len(self.data))) + 1
+    first_leaf = math.pow(2,depth - 1) - 1
+    while current_index < first_leaf:
+      child_a = self.data[current_index * 2 + 1]
+      if current_index * 2 + 2 >= len(self.data):
+        priority_child = child_a
+        priority_child_index = current_index * 2 + 1
+      else:
+        child_b = self.data[current_index * 2 + 2]
+        if child_a[1] > child_b[1]:
+          priority_child = child_a
+          priority_child_index = current_index * 2 + 1
+        else:
+          priority_child = child_b
+          priority_child_index = current_index * 2 + 2
+      if self.data[current_index][1] < priority_child[1]:
+        old_current = self.data[current_index]
+        self.data[current_index] = self.data[priority_child_index]
+        self.data[priority_child_index] = old_current
+      else:
+        return
+  def insert(self,item):
+    self.data.append(item)
+    self.bubble_up(len(self.data) - 1)
+  def pop(self):
+    if len(self.data) == 0:
+      return None
+    last_leaf = self.data.pop()
+    if len(self.data) == 0:
+      return last_leaf
+    else:
+      old_root = self.data[0]
+      self.data[0] = last_leaf
+      self.push_down(0)
+      return old_root
+
+node = NumberNode(0,[0,0,1],[8,9,12],None)
+#print(monte_carlo_from_node(node,[Card(0,8),Card(0,9)],3,[Card(1,12)],500))
+print(node)
+found = djikstra_mod_list(node,10)
+for found_node in found:
+  print(monte_carlo_from_node(found_node,4,500))
+
+"""h = Heap([("a",2),("b",5),("c",1),("d",6)])
+print(h.pop())
+print(h.pop())
+print(h.pop())
+print(h.pop())"""
